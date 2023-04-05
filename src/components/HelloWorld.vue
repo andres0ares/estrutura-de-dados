ba196544b3e310a574f40f8de95272bf1fc9d90a
Andre Soares <andreiarley@gmail.com>
2023-04-05 19:22:59 -0300

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container class="fill-height">
+    <div>
+      <v-btn class="blue" color="blue">clicar
+      </v-btn>
+    </div>
+  </v-container>
+</template>
+
+<script setup>
+  //
+</script>
---
+++ src/views/Sequencial.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="main">
+    <div>
+      <p class="text-h5">Lista Sequencial {{ count }}</p>
+      <v-slider
+        v-model="count"
+        class="slider"
+        :max="30"
+        :min="5"
+        step="1"
+      ></v-slider>
+      <v-btn @click="handleTest" color="black">iniciar</v-btn>
+    </div>
+    <div>
+      <div v-for="box in array" class="float-left box">
+        <p>{{ box.value }} - {{ box.idx }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+      array: [],
+    };
+  },
+  methods: {
+    handleTest() {
+      for (let i = 0; i < this.count; i++)
+        this.array.push({ value: "none", idx: i });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.main {
+  background-color: #e5e5e5;
+  min-height: 100vh;
+  padding: 4rem 0;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.slider {
+  width: 200px;
+}
+
+.box {
+  border: 2px solid black;
+}
+</style>
